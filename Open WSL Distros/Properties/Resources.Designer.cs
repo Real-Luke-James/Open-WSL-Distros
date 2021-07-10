fc0abda44689e1e90b14fcee12930526bd8cb634
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2021-07-10 16:55:53 -0700

    test5

--- Open WSL Distros/Properties/Resources.Designer.cs
+++ Open WSL Distros/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Open_WSL_Distros.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -86,6 +86,15 @@ namespace Open_WSL_Distros.Properties {
             get {
                 object obj = ResourceManager.GetObject("openSUSE", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to e.
+        /// </summary>
+        internal static string test {
+            get {
+                return ResourceManager.GetString("test", resourceCulture);
             }
         }
         
