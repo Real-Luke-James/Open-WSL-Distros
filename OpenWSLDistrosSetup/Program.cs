7f241a0bcb38c85b91f20b92b189efeb2228727a
Real-Luke-James <79417248+Real-Luke-James@users.noreply.github.com>
2021-07-10 15:54:45 -0700

    Update Setup File

--- OpenWSLDistrosSetup/Program.cs
+++ OpenWSLDistrosSetup/Program.cs
@@ -7,7 +7,11 @@ namespace OpenWSLDistrosSetup
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Welcome to the Open WSL Distros Setup.");
-			System.Diagnostics.Process.Start("cmd.exe", "./test.bat");
+			Console.WriteLine("Before Starting, Please confirm that PowerShell 6+ Is Installed. If it isn't installed, please go to https://github.com/powershell/powershell/ and download the newest release.");
+			Console.WriteLine("Press any key to start part 1 of setup.");
+			System.Diagnostics.Process.Start("cmd,exe", "pause");
+			System.Diagnostics.Process.Start("cmd.exe", "./setup.bat");
+			Console.WriteLine("Part 1 of setup has ");
 		}
 	}
 }
